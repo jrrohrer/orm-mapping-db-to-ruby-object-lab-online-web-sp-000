2dd7d0ef0035eba78b4bcf51dff3bf49c49b626e
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-12-28 19:51:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,8 +85,15 @@ class Student
     end.first
   end
   
-  def self.all_students_in_grade_X
-    
+  def self.all_students_in_grade_X(grd)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+     DB[:conn].execute(sql, grd).map do |row|
+       self.new_from_db(row)
+     end
   end
   
   def save
